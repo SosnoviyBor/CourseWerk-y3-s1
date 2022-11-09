acc51d19827a375d9c90615cfa722b734495f24c
SosnoviyBor <wormer007@gmail.com>
2022-11-09 22:23:10 +0200

    Changed (and fixed) search behaviour

---
+++ cw/static/cw/home.js
@@ -0,0 +1,70 @@
+// const host = "http://127.0.0.1:8000/"
+
+const search = document.getElementById("search")
+const allItems = Array.from(document.getElementsByClassName("item"))
+// Функціонал пошуку
+search.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        // Блокуємо звичайну поведінку натиснутої клавіші
+        event.preventDefault()
+
+        const inp = search.value.toLowerCase()
+        const result = []
+        search.value = ""
+
+        // Якщо результат відсутній, нічого не робимо
+        if (inp === "") { return }
+
+        // Видалення усіх сторінок, що були присутні
+        try {
+            // Та повідомлення, якщо воно існувало
+            document.getElementById("not-found").remove()
+        } catch {
+            Array.from(document.getElementsByClassName("item"))
+                .forEach(book => {book.remove()})
+        }
+
+        // Фільтрування збережених у пам'ять сторінок
+        for (let i = 0; i < allItems.length; i++){
+            const name = allItems[i].getElementsByClassName("item-name")[0].innerHTML
+            const desc = allItems[i].getElementsByClassName("item-desc")[0].innerHTML
+            if (name.toLowerCase().includes(inp) ||
+                desc.toLowerCase().includes(inp)) {
+                    result.push(allItems[i])
+            }
+        }
+
+        // Відображення результату пошуку
+        if (result.length === 0) {
+            // Нічого не було знайдено
+            const content = document.getElementById("item-list")
+            const p = document.createElement("p")
+            p.id = "not-found"
+            p.innerHTML = "Нажаль, нічого не було знайдено за вашим запитом"
+            content.appendChild(p)
+        } else {
+            // Щось було знайдено
+            for (let i = 0; i < result.length; i++){
+                document.getElementById("item-list").appendChild(result[i])
+            }
+        }
+
+
+        //  Збережено на випадок, якщо треба буде робити пошук через бекенд
+        // Формуємо GET запит та відправляємо на вьюху, що прив'язана до "HOST/" сторінки
+        // TODO Після першого визову обнулює фільтри
+        // Насправді, досить погано процює
+        // $.ajax({
+        //     url: `${host}search/`,
+        //     type: "GET",
+        //     data: {
+        //         "action": "search",
+        //         "value": search.value
+        //     },
+        //     success: (data)=>{
+        //         window.location.href = host
+        //         document.write(data)
+        //     }
+        // })
+    }
+})
