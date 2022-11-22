e3849ccff37fd140149511430857ca27afb5815b
SosnoviyBor <wormer007@gmail.com>
2022-11-23 01:12:22 +0200

    Idk why I made this field float in the first place

--- cw/models.py
+++ cw/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import CheckConstraint, Q
 from datetime import date
 
 # Create your models here.
@@ -7,7 +9,8 @@ from datetime import date
 class Page(models.Model):
     head = models.TextField()
     desc = models.TextField()
-    img = models.TextField(blank=False) # Зображення має бути щонайменш 300 пікселів у висоту
+    # Зараз відображення альбомних зображень у картках криве, тому майте це на увазі
+    img = models.TextField(blank=False)
     data = models.TextField()
     author = models.CharField(max_length=100)
     date_created = models.DateField(default=date.today)
@@ -15,10 +18,15 @@ class Page(models.Model):
 class PageRelation(models.Model):
     which = models.ForeignKey(Page, related_name="which", on_delete=models.CASCADE)
     comes_after = models.ForeignKey(Page, related_name="comes_after", on_delete=models.CASCADE)
-    strength = models.FloatField()
+    strength = models.IntegerField(     # Обмеження на рівні моделі
+        validators=[MaxValueValidator(100), MinValueValidator(0)])
 
     class Meta:
         unique_together = [["which", "comes_after"]]
+        constraints = ([     # Обмеження на рівні БД
+            CheckConstraint(
+                check=Q(strength__gte=0) & Q(strength__lte=100),
+                name='strength_range')])
 
 class Folder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
